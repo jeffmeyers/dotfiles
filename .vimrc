2cb32edf89c0872a10cf902c691577fdf465f7a5
Jeff Meyers <jeffreym.meyers@gmail.com>
2015-10-10 13:41:39 +0200

    Simplified vimrc, start of new Rubby setup script

--- .vimrc
+++ .vimrc
@@ -1,61 +1,22 @@
-filetype on 
-set nocompatible 
-
-" Minibuffer Explorer Settings 
-let g:miniBufExplMapWindowNavVim = 1 
-let g:miniBufExplMapWindowNavArrows = 1 
-let g:miniBufExplMapCTabSwitchBufs = 1 
-let g:miniBufExplModSelTarget = 1 
-
-set cf " ask about saving changes
-set clipboard+=unnamed
-set history=256 
-set ruler  
-set nu  
-set timeoutlen=250  
-
-set ts=2 
-set bs=2  
-set shiftwidth=2  
-set smartindent
-set autoindent
-set smarttab
-set expandtab
-
-set showmatch  
-set mat=5  
-set novisualbell  
-set noerrorbells  
-set laststatus=2  
-set statusline=%-10.3n\ %t\ %y\ %P\ %{fugitive#statusline()} 
-set mouse=a
-set hlsearch
-
 execute pathogen#infect()
+syntax on
+set number
+set incsearch
+set hlsearch
+set expandtab
+set tabstop=2
+set shiftwidth=2
 
-" NERDTree mappings
+let mapleader = ","
+map <Leader>f :CtrlP<CR>
+map <Leader>b :CtrlPBuffer<CR>
 map <Tab> :NERDTreeToggle<CR>
-
-" tab mappings
-map <C-j> :tabp<CR>
-map <C-k> :tabn<CR>
 map <C-t> :tabnew<CR>
-map <C-x> :q<CR>
-map <C-\> :vimgrep <cword> **/*.rb **/*.coffee **/*.feature <CR> :cwindow <CR>
-
-syntax enable
-set background=dark
-let g:solarized_termtrans = 1
-colorscheme solarized
-
-" backup
-set backupdir=~/tmp
-set swapfile
-set dir=~/tmp
-
-let mapleader=","
-
-filetype plugin indent on
+map <C-k> :tabn<CR>
+map <C-j> :tabp<CR>
+map <C-x> :tabclose<CR>
 
 " open quickbuf results in a new tab
 set switchbuf+=newtab
+
+filetype plugin indent on
---
+++ setup.rb
@@ -0,0 +1,75 @@
+def log(msg)
+	puts '='*100
+	puts msg
+	puts '='*100
+end
+
+# create and navigate to setup tmp dir
+log 'Creating temp directory'...
+`mkdir -p ~/setup_tmp`
+`cd ~/setup_tmp`
+
+# install ohmyzsh
+log 'Installing OhMyZsh...'
+`sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
+
+# install homebrew
+log 'Installing Homebrew...'
+`ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+
+# install cask
+log 'Installing Cask...'
+`brew install caskroom/cask/brew-cask`
+
+# install basic utilities
+log 'Installing basic utilities...'
+`brew install git htop vim tree tmux wget ack nvm postgresql`
+
+# install cask utilities
+log 'Installing cask utilities...'
+`brew cask install virtualbox vagrant appcleaner coconutbattery`
+
+# set up node
+log 'Setting up NodeJS...'
+`mkdir -p ~/.nvm`
+`cp $(brew --prefix nvm)/nvm-exec ~/.nvm/`
+`echo 'export NVM_DIR=~/.nvm' >> ~/.zshrc`
+`echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.zshrc`
+`source ~/.zshrc`
+`nvm install stable`
+`nvm use stable`
+
+# install rbenv
+log 'Installing rbenv...'
+`git clone https://github.com/sstephenson/rbenv.git ~/.rbenv`
+`echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc`
+`echo 'eval "$(rbenv init -)"' >> ~/.zshrc`
+`source ~/.zshrc`
+`git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build`
+
+# install ruby
+log 'Installing ruby...'
+`rbenv install -s 2.1.2`
+
+# install pathogen
+log 'Installing pathogen...'
+`mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim`
+`echo 'execute pathogen#infect()' >> ~/.vimrc`
+
+# install vim plugins
+log 'Installing vim plugins...'
+plugins = [
+	'https://github.com/kien/ctrlp.vim.git',
+	'https://github.com/scrooloose/nerdtree.git'
+]
+plugins.each do |plugin|
+  log "Installing #{plugin}..."
+  %x{ cd ~/.vim/bundle && git clone #{plugin} && cd - }
+end
+
+# install smyck
+log 'Installing SMYCK color theme...'
+`mkdir -p ~/.vim/colors`
+`wget https://raw.githubusercontent.com/hukl/Smyck-Color-Scheme/master/smyck.vim -O ~/.vim/colors/smyck.vim`
+`wget https://raw.githubusercontent.com/hukl/Smyck-Color-Scheme/master/Smyck.terminal -O ~/setup_tmp/smyck.terminal`
+`open ~/setup_tmp/smyck.terminal`

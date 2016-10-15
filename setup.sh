c71365df951065d8815f8f7da4ec82325b819272
Jeff Meyers <jeffreym.meyers@gmail.com>
2016-10-14 23:28:52 -0500

    Update brew packages

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install caskroom/cask/brew-cask
 
 # install basic utilities
-brew install git htop vim tree tmux wget ack nvm postgresql reattach-to-user-namespace fzf neovim/neovim/neovim --verbose
+brew install git htop vim tree wget ag nvm postgresql fzf neovim/neovim/neovim --verbose
 
 # install fzf shell extensions
 /usr/local/opt/fzf/install --all
